3df6fcf61d84c27ecc30b4682b26a759856bc301
Willowthedeku <96545539+Willowthedeku@users.noreply.github.com>
2026-01-25 15:17:44 -0700

    Add files via upload

---
+++ varaibles.php
@@ -0,0 +1,20 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+	<meta charset="utf-8">
+ 	<title>Variables</title>
+</head>
+<body>
+<?php
+	$first = "William";
+	$last = "Sawdy";
+	$street = "6928 Johnsonville Wy";
+	$city = "Sheboygan Falls";
+	$state = "WI";
+	$zip = 53085;
+
+	print "<p>$first $last address is:<br>$street
+	<br>$city $state $zip</p>";
+?>
+</body>
+</html>
