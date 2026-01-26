6afc3f83135aec6ccb8fb1f257ed049d4b0cae4b
Willowthedeku <96545539+Willowthedeku@users.noreply.github.com>
2026-01-25 18:13:10 -0700

    Add files via upload

---
+++ predefined.php
@@ -0,0 +1,15 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<title>Predefined Variables</title>
+</head>
+<body>
+<pre>
+<?php
+	variable:
+	print_r($_SERVER);
+?>
+</pre>
+</body>
+</html>
