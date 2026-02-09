d91cb7f4397b873bda06a1f0e94b891caf10d837
Willowthedeku <96545539+Willowthedeku@users.noreply.github.com>
2026-02-08 20:59:02 -0700

    Add files via upload

---
+++ handle_reg3.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+	<style type="text/css" media="screen">
+		.error { color: red; }
+	</style>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php 
+$okay = true;
+
+error_reporting(E_ALL);
+
+if (empty($_POST['email'])) {
+	print '<p class="error">Please enter your email address.</p>';
+	$okay = false;
+}
+
+if (empty($_POST['password'])) {
+	print '<p class="error">Please enter your password.</p>';
+	$okay = false;
+}
+
+if (is_numeric($_POST['year'])) {
+	$age = 2026 - $_POST['year']; 
+} else {
+	print '<p class="error">Please enter the year you were born as four digits.</p>';
+	$okay = false;
+}
+
+if ($okay) {
+	print '<p>You have been successfully registered.</p>';
+	print "<p>You will turn $age this year.</p>";
+}
+?>
+</body>
+</html>
